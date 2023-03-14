ff6d7f6a6805b8ae04e37b6806447a9ba06ebd53
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-03-14 15:05:11 +0100

    added upload and download functions

---
+++ model/file.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+class File {
+  String fileName;
+  DateTime dateModified;
+  int size;
+
+  File({
+    required this.fileName,
+    required this.dateModified,
+    required this.size,
+  });
+
+  factory File.fromJson(Map<String, dynamic> json) {
+    //print( "Datum: ${json["dateModified"]}");
+
+    return new File(
+      fileName: json["fileName"],
+      dateModified: DateTime.parse(json["dateModified"]),
+      size: json["size"],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "fileName": fileName,
+        "dateModified": dateModified,
+        "size": size,
+      };
+}
+
+// USED https://app.quicktype.io/ TO CREATE THIS FILE FROM /api/file GET
+
+List<File> fileFromJson(String str) {
+  final jsonData = json.decode(str);
+  return new List<File>.from(jsonData.map((x) => File.fromJson(x)));
+}
+
+String fileToJson(List<File> data) {
+  final dyn = new List<dynamic>.from(data.map((x) => x.toJson()));
+  return json.encode(dyn);
+}
